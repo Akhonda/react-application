78c48986c8a9ab4cc559f5fc5d19a647d566a36d
Akhonda <mdmahmudhasanakhonda@gmail.com>
2024-12-20 03:13:39 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import Counter from './components/Counter'
+import Menu from './components/menu/Menu'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter/> */}
+      <Menu/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+const Menu = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="default.asp">Home</a>
+        </li>
+        <li>
+          <a href="news.asp">News</a>
+        </li>
+        <li>
+          <a href="contact.asp">Contact</a>
+        </li>
+        <li>
+          <a href="about.asp">About</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
