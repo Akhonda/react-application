5fb2ec4c26e5525e1151f2f3bb9314be45e6bd0f
abufahimkhan <abufahimkhan1@gmail.com>
2024-12-21 21:39:43 +0600

    Menu bar updated

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,23 +1,68 @@
-// import React from 'react'
-import "../../App.css"
+// Menu.jsx
+import "../../App.css";
+import Logo from "../../assets/logo.png";
+
 const Menu = () => {
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <header className="menu-header">
+      <div className="menu-container">
+        {/* Logo */}
+        <a href="/" className="menu-logo">
+          <img src={Logo} alt="Logo" />
+        </a>
+
+        {/* Navigation */}
+        <nav className="menu-nav">
+          <ul>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Careers</a>
+            </li>
+            <li>
+              <a href="/">History</a>
+            </li>
+            <li>
+              <a href="/">Services</a>
+            </li>
+            <li>
+              <a href="/">Projects</a>
+            </li>
+            <li>
+              <a href="/">Blog</a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Buttons */}
+        <div className="menu-buttons">
+          <a href="/" className="login-btn">
+            Login
+          </a>
+          <a href="/" className="register-btn">
+            Register
+          </a>
+        </div>
+
+        {/* Mobile Menu Toggle */}
+        <button className="menu-toggle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
   );
 };
 
